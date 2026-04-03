91983bc8b336b4167c9f8877e1c548e75d96b519
Toqa Ashraf <toqaaashraf87@gmail.com>
2026-04-03 09:35:24 +0200

    Final Update README

--- Backend/WebApp1/WebApp1/Models/ClientBookings.cs
+++ Backend/WebApp1/WebApp1/Models/ClientBookings.cs
@@ -1,6 +1,11 @@
-﻿namespace WebApp1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Models
 {
     public class ClientBookings
     {
+        [Key]
+        public int BookingID { get; set; }
+
     }
 }
